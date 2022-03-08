ea401d7b1833ba8ecb393280a5f4b1131c74bc82
“username” <angeljunotruffles@gmail.com>
2022-03-08 01:37:31 -0500

    wanted to decrememnt on the last bonus, but finished

---
+++ script.js
@@ -0,0 +1,20 @@
+//1-9
+//const
+let num = 20;
+num = 15;
+num = num + 7;
+num--;
+num = 'This is not a number!';
+let blank ;
+blank = null;
+
+console.log(num);
+console.log(blank);
+
+//BONUS
+const [quarter , nickel , dime , penny] = [.25 , .05 , .10 , .01];
+let money = [quarter, nickel, dime, penny]
+console.log(money);
+let letters = 'qwertyuiop'
+console.log((letters.length)-1);
+console.log(letters [(letters.length-1)]);
